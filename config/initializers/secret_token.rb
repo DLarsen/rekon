609d37547b06248357bdde9c41dbd4e7e696d30f
David <david@david-VirtualBox.(none)>
2014-02-21 15:13:47 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rekon::Application.config.secret_key_base = 'f64af7032a3e253c03ca3a14c58437c1c08d5f72aa3ada3499752b44164b7789424e11c22ecc729e77ebf9f030f5333511d1bbe5db682ef9bec837d25491039b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rekon::Application.config.session_store :cookie_store, key: '_rekon_session'
