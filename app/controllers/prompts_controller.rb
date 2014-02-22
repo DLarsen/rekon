727daf05b404b923fa42b53139a61da40daa95fc
Nic DeVos <nic@devos.me>
2014-02-22 10:53:43 -0800

    fixed incorrect column prompt name

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -2,7 +2,7 @@ class PromptsController < ProjectBaseController
 
   def index
     @prompts = Prompt.all
-    @replies = Hash[current_project.replies.where.not(complete?:true).map{|r| [r.prompt.id, r]}]
+    @replies = Hash[current_project.replies.where.not(complete:true).map{|r| [r.prompt.id, r]}]
   end
 
   def show
