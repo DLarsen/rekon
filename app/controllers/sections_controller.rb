ad005fc3dadc572bec3cfeb659040e962cf28200
David <david@david-VirtualBox.(none)>
2014-02-23 00:58:17 -0800

    try to do the right thing if we click back to a "complete" section

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -13,7 +13,11 @@ class SectionsController < ProjectBaseController
     # if @section.level == 1 && @section.first_child
     #   redirect_to @section.first_child
     # else
-    if @section.children.size == 0 && @section.prompts.size > 0
+
+    section_status = current_project.section_statuses.where('section_id = ?', @section.id).first
+    is_complete = section_status && section_status.complete
+
+    if @section.children.size == 0 && @section.prompts.size > 0 && !is_complete
       n = next_prompt(current_project, @section)
       redirect_to prompt_url(n.id)
     else
